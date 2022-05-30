0f55205e439d1dde7119c58983973163f906813a
mecenas <jrafael.pfz@gmail.com>
2022-05-30 17:30:46 -0400

    [feature] - implement validation to forms using express-validator

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,7 +1,13 @@
-const usersRepo = require('../../repositories/users');
 const express = require('express');
+const usersRepo = require('../../repositories/users');
+const { check, validationResult } = require('express-validator');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation,
+} = require('./validator.js');
 
 const router = express.Router();
 
@@ -9,23 +15,23 @@ router.get('/signup', (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-  const existingUser = await usersRepo.getOneBy({ email });
+router.post(
+  '/signup',
+  [requireEmail, requirePassword, requirePasswordConfirmation],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
 
-  if (existingUser) {
-    return res.send('Email in use');
-  }
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match');
-  }
-  // crear un usuario en nuestro repo para respresentarlo
-  const user = await usersRepo.create({ email, password });
+    const { email, password } = req.body;
 
-  // guardar el Id del usuario en un cookie
-  req.session.userId = user.Id; // propiedad que viene de cookie session
-  res.send('account created');
-});
+    // crear un usuario en nuestro repo para respresentarlo
+    const user = await usersRepo.create({ email, password });
+
+    // guardar el Id del usuario en un cookie
+    req.session.userId = user.Id; // propiedad que viene de cookie session
+    res.send('account created');
+  }
+);
 
 router.get('/signout', (req, res) => {
   req.session = null;
---
+++ routes/admin/validators.js
@@ -0,0 +1,31 @@
+const { check, validationResult } = require('express-validator');
+const usersRepo = require('../../repositories/users');
+
+module.exports = {
+  requireEmail: check('email')
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Must be valid Email')
+    .custom(async (email) => {
+      const existingUser = await usersRepo.getOneBy({ email });
+      if (existingUser) {
+        throw new Error('Email in use');
+      }
+    }),
+  requirePassword: check('passwordConfirmation')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Must be valid Email')
+    .withMessage('Must be between 4 and 20 characters'),
+  requirePasswordConfirmation: check('passwordConfirmation')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Must be valid Email')
+    .withMessage('Must be between 4 and 20 characters')
+    .custom(async (password, { req }) => {
+      if (passwordConfirmation !== req.body.password) {
+        throw new Error('Passwords must match');
+      }
+    }),
+};
