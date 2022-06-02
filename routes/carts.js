0b03600e736210008f208cc2a4afbb4020a815df
mecenas <jrafael.pfz@gmail.com>
2022-06-02 17:29:02 -0400

    [feature] - implement show products in cart + styles & display quantity + total price

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -36,6 +38,19 @@ router.post('/cart/products', async (req, res) => {
 });
 
 // receive a GET request to show all items in cart
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect('/');
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.Id);
+    item.product = product;
+  }
+  res.send(cartShowTemplate({ items: cart.items }));
+});
 
 // receive a post request to delete an item from a cart
 
